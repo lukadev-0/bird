c92123e7aa80b6325e314e02a3cfc94f83423d8b
Luka <47296785+lukadev-0@users.noreply.github.com>
2023-06-30 20:19:26 +0200

    feat: update notifications page

--- src/app/(app)/notifications/page.tsx
+++ src/app/(app)/notifications/page.tsx
@@ -1,3 +1,7 @@
 export default function Notifications() {
-  return <div>nothing</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Notifications don&apos;t exist yet lol</p>
+    </div>
+  );
 }
