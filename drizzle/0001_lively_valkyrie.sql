5408e00744516fabed0342f40033dad6c67be8c7
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2023-10-31 13:35:12 +0100

    plez work

--- drizzle/0001_lively_valkyrie.sql
+++ drizzle/0001_lively_valkyrie.sql
@@ -1,7 +1,27 @@
-ALTER TABLE posts ADD COLUMN `parent_id` text;--> statement-breakpoint
-ALTER TABLE posts ALTER COLUMN `parent_id` TO `parent_id` text REFERENCES posts(id);--> statement-breakpoint
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+
+BEGIN;--> statement-breakpoint
+
+CREATE TABLE `new_posts` (
+	`id` text PRIMARY KEY NOT NULL,
+	`created_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	`author_id` text NOT NULL,
+	`content` text NOT NULL,
+	FOREIGN KEY(parent_id) REFERENCES posts(id)
+);--> statement-breakpoint
+
+INSERT INTO `new_posts` SELECT * FROM `posts`;--> statement-breakpoint
+DROP TABLE `posts`;--> statement-breakpoint
+ALTER TABLE `new_posts` RENAME TO `posts`;--> statement-breakpoint
+DROP TABLE `posts`;--> statement-breakpoint
+
 CREATE INDEX `parent_id_idx` ON `posts` (`parent_id`);--> statement-breakpoint
 CREATE INDEX `created_at_idx` ON `posts` (`created_at`);--> statement-breakpoint
+
+PRAGMA foreign_key_check;--> statement-breakpoint
+COMMIT;--> statement-breakpoint
+
+PRAGMA foreign_keys=ON;--> statement-breakpoint
 /*
  SQLite does not support "Creating foreign key on existing column" out of the box, we do not generate automatic migration for that, so it has to be done manually
  Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
